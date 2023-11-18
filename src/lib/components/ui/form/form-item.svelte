ee399dc862b0ddff98df2a43f4bac03575bd521e
phuc1nguyen <phuc.ng13988@gmail.com>
2023-11-18 10:14:36 +0700

    Re-add shadcn components

--- src/lib/components/ui/form/form-item.svelte
+++ src/lib/components/ui/form/form-item.svelte
@@ -3,11 +3,10 @@
 	import type { HTMLAttributes } from "svelte/elements";
 
 	type $$Props = HTMLAttributes<HTMLDivElement>;
-
-	let className: $$Props["class"] = undefined;
+	let className: string | undefined | null = undefined;
 	export { className as class };
 </script>
 
-<div class={cn("p-6 pt-0", className)} {...$$restProps}>
+<div class={cn("space-y-2", className)} {...$$restProps}>
 	<slot />
 </div>

86895a28bcbf6bc4a1e90015c4f7ffe672adf6b0
phuc1nguyen <phuc.ng13988@gmail.com>
2023-11-19 09:57:12 +0700

    Extract user to pages from locals only in certain routes group
    Should be part of commit 'Enhance client-side authentication and routes protection

--- src/routes/(visitors)/+page.server.ts
+++ src/routes/(visitors)/+page.server.ts
@@ -1,11 +1,15 @@
 import { redirect, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
 import * as authService from '$lib/services/auth.service';
+
+export const load: PageServerLoad = ({ locals }) => ({ user: locals.user });
 
 export const actions = {
   default: async ({ cookies }) => {
     await authService.terminateSession();
 
-    cookies.delete('user', { path: '/' });
+    cookies.delete('resutnerruc', { path: '/' });
     cookies.delete('connect.sid', { path: '/' });
 
     throw redirect(302, '/');
